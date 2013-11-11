cd5e56a72c189345e161e0f3ae9b180ad84d88eb
hempywh <will.hempy@gmail.com>
2013-11-11 16:32:27 -0600

    fixed those files

--- Digits.java
+++ Digits.java
@@ -7,7 +7,57 @@ public class Digits {
 	
 	//array [][] of F_ij (pixel) probabilities
 	//for all 0-9 digits
+	
+	public Digits() {
+	FileReader inputStream = null;
+    
+    try {
+        inputStream = new FileReader("digitdata/testimages");
+       
+        int c;
+        
 
+       
+        outerloop:
+        while(true)
+        {
+			for(int l =0; l < 29; l++)
+			{
+				c =  inputStream.read();
+				
+				if(c == -1)
+				{
+					break outerloop;
+				}
+				
+				if(c != -1)
+				{	
+    				if(c == 32)
+    					System.out.print( " " );
+    				else if(c == 35)
+    					System.out.print("#");
+    				else if(c == 43)
+    					System.out.print("+");
+    				
+				}	
+						
+			}
+			System.out.println();
+			
+			//new letter
+			
+			
+			
+		}
+
+	} 
+	catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+	
+	
+	
+}
 	
 
 }
--- Main.java
+++ Main.java
@@ -7,6 +7,6 @@ public class Main {
 
 public static void main(String[] args) throws IOException {
 	Digits digit_classification = new Digits();
-
+	
 	}
 }
