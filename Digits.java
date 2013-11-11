bebf6ead1986a4e51cef5c36da68f2d6875cadea
hempywh <will.hempy@gmail.com>
2013-11-11 16:26:22 -0600

    added your files

---
+++ Digits.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Digits {
+	
+	//array [][] of F_ij (pixel) probabilities
+	//for all 0-9 digits
+
+	
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Main {
+
+public static void main(String[] args) throws IOException {
+	Digits digit_classification = new Digits();
+
+	}
+}
